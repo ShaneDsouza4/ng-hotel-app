2c0739f81fb440836fff0c0274e239272e9314fe
ShaneDsouza4 <shane.json96@gmail.com>
2025-05-23 01:48:04 -0400

    Reservation App CRUD Completed

---
+++ src/app/reservation/reservation.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+
+  private reservations: Reservation[] = [];
+
+  constructor() {
+    let savedReservations = localStorage.getItem("reservations");
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
+  // CRUD
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(x => x.id == id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    reservation.id = Date.now().toString();
+    this.reservations.push(reservation);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(x => x.id == id);
+    this.reservations.splice(index, 1);
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  }
+
+  updateReservation(id: string, updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(x => x.id == id);
+    this.reservations[index] = updatedReservation;
+    localStorage.setItem("reservations", JSON.stringify(this.reservations));
+  }
+
+}
